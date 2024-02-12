a0b88db8e87bca39aaed8486047cffa5cfc15b3b
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2024-02-12 10:23:40 -0500

    use default mysql configs

--- config.py
+++ config.py
@@ -15,14 +15,13 @@ class Config:
     LOG_LEVEL = env.get("LOG_LEVEL", "DEBUG")
     LOG_FILE = env.get("LOG_FILE", "/var/logs/webapp_aws_cicd.log")
     DEBUG = env.get("DEBUG", True)
-    MYSQL_USER = env.get("MYSQL_USER", "test")
-    MYSQL_PASSWORD = env.get("MYSQL_PASSWORD", "test@123")
+    MYSQL_USER = env.get("MYSQL_USER", "root")
+    MYSQL_PASSWORD = env.get("MYSQL_PASSWORD", "root")
     MYSQL_HOST = env.get("MYSQL_HOST")
-    MYSQL_DB = env.get("MYSQL_DB", "DB")
+    MYSQL_DB = env.get("MYSQL_DB", "application")
     MYSQL_PORT = env.get("MYSQL_PORT", "3306")
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
     FLASK_APP = env.get("FLASK_APP")
-    FLASK_RUN_PORT = int(env.get("FLASK_RUN_PORT"))
 
 
 app_config = Config()
