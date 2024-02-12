6f90afcce5d15d25f81311d9611f44c33874ee70
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2024-02-12 12:24:14 -0500

    get env from secrets

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ env = os.environ
 
 
 class Config:
-    LOG_LEVEL = env.get("LOG_LEVEL", "DEBUG")
+    LOG_LEVEL = env.get("LOG_LEVEL", "INFO")
     LOG_FILE = env.get("LOG_FILE", "/var/logs/webapp_aws_cicd.log")
-    DEBUG = env.get("DEBUG", True)
-    MYSQL_USER = env.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = env.get("MYSQL_PASSWORD", "root")
+    DEBUG = env.get("DEBUG")
+    MYSQL_USER = env.get("MYSQL_USER")
+    MYSQL_PASSWORD = env.get("MYSQL_PASSWORD")
     MYSQL_HOST = env.get("MYSQL_HOST")
-    MYSQL_DB = env.get("MYSQL_DB", "application")
-    MYSQL_PORT = env.get("MYSQL_PORT", "3306")
+    MYSQL_DB = env.get("MYSQL_DB")
+    MYSQL_PORT = env.get("MYSQL_PORT")
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
     FLASK_APP = env.get("FLASK_APP")
 
